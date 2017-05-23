9544f20d21a2c3d113a8e9243c99c49310069022
Luiz Parreira <luizfilipester@gmail.com>
2017-05-23 09:51:17 -0300

    Initial package commit

---
+++ lib/ecto_castint.ex
@@ -0,0 +1,18 @@
+defmodule EctoCastint do
+  @moduledoc """
+  Documentation for EctoCastint.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EctoCastint.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/ecto_castint_test.exs
@@ -0,0 +1,8 @@
+defmodule EctoCastintTest do
+  use ExUnit.Case
+  doctest EctoCastint
+
+  test "the truth" do
+    assert 1 + 1 == 2
+  end
+end
